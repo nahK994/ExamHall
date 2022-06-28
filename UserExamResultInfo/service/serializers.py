24ed4a7776334d95d4b4a97991b1dc6843e0acf9
nahK994 <nkskl6@gmail.com>
2022-06-29 02:16:51 +0600

    remove unnecessary service

--- UserExamResultInfo/service/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from . import models
-
-class UserExamResultInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.UserExamResultInfoModel
-        fields = '__all__'
