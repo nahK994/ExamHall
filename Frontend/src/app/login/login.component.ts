8456610a90db4832e62d06baba1482a96e3ff339
nahK994 <nkskl6@gmail.com>
2022-07-24 00:37:27 +0600

    create login page

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  loginForm: FormGroup;
+  canDisplayRoleForm: boolean = false;
 
-  ngOnInit(): void {
+  constructor(
+    private _fb: FormBuilder
+  ) {
+    this.loginForm = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role:['']
+    })
+  }
+
+  setRole(role: string) {
+    this.loginForm.get('role')?.setValue(role);
+  }
+
+  checkCredential() {
+    if(this.loginForm.get('email')?.valid && this.loginForm.get('password')?.valid) {
+      this.canDisplayRoleForm = true;
+    }
   }
 
 }
--- Frontend/src/app/login/login.module.ts
+++ Frontend/src/app/login/login.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -12,6 +16,11 @@ import { LoginComponent } from './login.component';
   imports: [
     CommonModule,
     LoginRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class LoginModule { }
