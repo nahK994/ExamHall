db9118f10ddcf9458ddef31bef3734705b3915b0
nahK994 <nkskl6@gmail.com>
2022-07-24 01:04:55 +0600

    design registration in FE

--- Frontend/src/app/registration/registration.component.ts
+++ Frontend/src/app/registration/registration.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent {
 
-  constructor() { }
+  registrationForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private _fb: FormBuilder
+  ) {
+    this.registrationForm = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role:['', [Validators.required]]
+    })
   }
 
+  createUser() {
+
+  }
 }
--- Frontend/src/app/registration/registration.module.ts
+++ Frontend/src/app/registration/registration.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration.component';
 import { RegistrationService } from './registration.service';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -11,7 +17,13 @@ import { RegistrationService } from './registration.service';
   ],
   imports: [
     CommonModule,
-    RegistrationRoutingModule
+    RegistrationRoutingModule,
+    MatSelectModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [
     RegistrationService
