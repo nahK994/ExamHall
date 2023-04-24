ce0b8ae33f7367206e6c1e4e046fba0845705af4
nahK994 <nkskl6@gmail.com>
2023-04-24 12:22:22 +0600

    add STATIC_ROOT in django settings

--- Backend/App/settings.py
+++ Backend/App/settings.py
@@ -169,19 +169,8 @@ SWAGGER_SETTINGS = {
             'type': 'apiKey',
             'in': 'header',
             'name': 'Authorization'
-        },
-        # "exclude_namespaces": [],  # List URL namespaces to ignore
-        # "api_version": '0.1',  # Specify your API's version
-        # "api_path": "/",  # Specify the path to your API not a root level
-        # "enabled_methods": [  # Specify which methods to enable in Swagger UI
-        #     'get',
-        #     'post',
-        #     'put',
-        #     'patch',
-        #     'delete'
-        # ],
-        # "api_key": '',  # An API key
-        # "is_authenticated": False,  # Set to True to enforce user authentication,
-        # "is_superuser": False,  # Set to True to enforce admin only access
+        }
     },
 }
+
+STATIC_ROOT='/home/examhall/ExamHall/static'
