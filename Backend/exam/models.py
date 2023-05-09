0456a5c2037782593ddf614f2c6f4253a95e09d4
nahK994 <nkskl6@gmail.com>
2023-05-09 20:26:19 +0600

    add exam date and duration in backend

--- Backend/exam/models.py
+++ Backend/exam/models.py
@@ -8,6 +8,8 @@ class ExamModel(models.Model):
     number_for_incorrect_answer = models.FloatField(default=0)
     number_of_seats = models.IntegerField(default=0)
     questions = models.ManyToManyField(QuestionModel, related_name="exams")
+    date = models.DateField()
+    duration = models.TimeField()
 
     def __str__(self):
         return f"{self.name} ({self.id})"
--- Backend/exam/serializers.py
+++ Backend/exam/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from datetime import datetime
 from question.models import TopicModel
 from .models import ExamModel
 from question.serializers import QuestionQuerySerializer
@@ -17,11 +17,18 @@ class ExamSerializer(serializers.ModelSerializer):
     numberForCorrectAnswer = serializers.IntegerField(source="number_for_correct_answer", min_value=1)
     numberForIncorrectAnswer = serializers.FloatField(source="number_for_incorrect_answer", max_value=0)
     numberOfSeats = serializers.IntegerField(source="number_of_seats", min_value=1)
+    date = serializers.CharField()
+    duration = serializers.CharField()
 
     class Meta:
         model = ExamModel
         fields = ['name', 'numberForCorrectAnswer', 'numberForIncorrectAnswer', 'numberOfSeats',
-                  'questions']
+                  'questions', 'date', 'duration']
+
+    def validate(self, validated_data):
+        validated_data['date'] = datetime.strptime(validated_data['date'], "%Y-%m-%d").date()
+        validated_data['duration'] = datetime.strptime(validated_data['duration'], "%H:%M").time()
+        return validated_data
 
 
 class ExamQuerySerializer(serializers.ModelSerializer):
@@ -34,7 +41,7 @@ class ExamQuerySerializer(serializers.ModelSerializer):
     class Meta:
         model = ExamModel
         fields = ['examId', 'name', 'numberForCorrectAnswer', 'numberForIncorrectAnswer', 'numberOfSeats',
-                  'questions']
+                  'questions', 'date', 'duration']
 
     def to_representation(self, instance):
         data = {
@@ -44,6 +51,8 @@ class ExamQuerySerializer(serializers.ModelSerializer):
             'numberForIncorrectAnswer': instance.number_for_incorrect_answer,
             'numberOfSeats': instance.number_of_seats,
             'questions': QuestionQuerySerializer(instance.questions, many=True).data,
+            'date': instance.date,
+            'duration': instance.duration,
             'topics': []
         }
 
