30c384e414c5611ac63fe317ff2017fd0d6e1a51
nahK994 <nkskl6@gmail.com>
2023-05-11 20:53:19 +0600

    separate setting information in different module in backend

--- Backend/App/settings.py
+++ Backend/App/settings.py
@@ -87,22 +87,22 @@ WSGI_APPLICATION = 'App.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'exam_hall',
-#         'USER': 'skhan',
-#         'PASSWORD': 'haha',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'exam_hall',
+        'USER': 'skhan',
+        'PASSWORD': 'haha',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- Backend/manage.py
+++ Backend/manage.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+load_dotenv()
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'App.settings')
+    settings_module = os.environ.get('SETTINGS_MODULE')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
