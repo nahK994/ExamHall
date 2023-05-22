02bdaa48e299f59b3a740b3c21c7a7daa140a6ef
Sumiya-Sayeed <sumiyasayeed@gmail.com>
2023-05-22 22:59:54 +0600

    Setup routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,15 +1,67 @@
-import { useState } from 'react'
+import {
+	BrowserRouter as Router, Route, Routes
+} from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { CssBaseline } from '@mui/material';
 
-  return (
-    <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import Login from './pages/login';
 
-export default App
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#e8f5e9', //lighter green
+			dark: '#1b5e20', //darker green
+			light: '#e8f5e9' //lightest green for poppers
+		},
+		secondary: {
+			main: '#616161',
+			dark: '#37474f', //darker color for font
+		},
+	},
+	components: {
+		// Name of the component
+		MuiButton: {
+			styleOverrides: {
+				// Name of the slot
+				root: {
+					// Some CSS
+					borderRadius: '5px'
+				},
+			},
+		}
+	}
+});
+
+const styles = {
+	'@global': {
+		'html, body, #root': {
+			width: '100%',
+			maxWidth: '100vw',
+			height: 'auto',
+			minHeight: '100vh',
+			// overflowX: 'hidden',
+			padding: 0,
+			margin: 0,
+			backgroundColor: 'white',
+		},
+	},
+};
+
+const App = () => {
+	return (
+    <ThemeProvider theme={theme}>
+							<CssBaseline />
+
+			<Router
+				basename="/"
+			>
+        <Routes location="/">
+							<Route index path="/" element={<Login />} />
+        </Routes>
+			</Router>
+      </ThemeProvider>
+	)
+};
+
+export default App;
