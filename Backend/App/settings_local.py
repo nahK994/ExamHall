2c6b37c8da14f89ec8e20f0d550b2c50f353862d
nahK994 <nkskl6@gmail.com>
2023-10-04 00:58:22 +0600

    docarize backend and db

--- Backend/App/settings_local.py
+++ Backend/App/settings_local.py
@@ -1,16 +1,14 @@
 from .settings import *
-from dotenv import load_dotenv
-load_dotenv(dotenv_path='../.env')
 import os
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'exam_hall'),
-        'USER': os.getenv('DB_USER', 'skhan'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'haha'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'NAME': 'exam_hall',
+        'USER': 'skhan',
+        'PASSWORD': 'haha',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
